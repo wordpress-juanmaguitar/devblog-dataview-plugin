45b6676bf9c9a7cddccd33bf4a2b5c76b87d85fb
JuanMa Garrido <juanma.garrido@gmail.com>
2024-08-07 12:11:01 +0100

    copy style.css to build folder as part of the build process

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import domReady from "@wordpress/dom-ready";
 import { createRoot } from "@wordpress/element";
 
-import "@wordpress/dataviews/build-style/style.css";
-
 import App from "./App";
 
 domReady(() => {
